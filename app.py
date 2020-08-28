8a96c338611b6d0b7a15c866bfe91115d2d41343
codetomath <69963212+codetomath@users.noreply.github.com>
2020-08-28 12:17:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import dash_html_components as html
 
 color_palette_blue_2 = ['#636EFA','#636EFA', '#636EFA', '#636EFA', '#636EFA', '#636EFA', '#636EFA', '#636EFA', '#636EFA', '#636EFA', '#636EFA', '#636EFA', '#1F1F25', '#636EFA']
 
-#df = pd.read_csv('https://raw.githubusercontent.com/codetomath/distressed_loans/master/Loans%20Breakdown.csv', sep = ';',encoding='ISO-8859-9')
 
 df= [
     ['2018-12',1.5],
@@ -25,9 +24,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
-
-
 df_2 = df
 
 df_2_ay_NPL = df_2.groupby(['Ay'])['NPL'].sum(),df_2.groupby(['Ay'])['NPL'].count()
@@ -41,31 +37,7 @@ df_2_ay_NPL['NPL'] = df_2_ay_NPL['NPL'].round(1)
 df_2_ay_NPL
 
 app.layout = html.Div([
-    
-
-   
-    
- #graph-output_03_orange 
-dcc.Graph(
-                        figure={
-                                    'data': [
-                                                {'x': (df_2_ay_NPL['Ay'].values.tolist()), 'y': (df_2_ay_NPL['NPL'].values.tolist()), 'type': 'bar', 'name': df_2_ay_NPL['Ay'].values.tolist(), 'marker' : {'color': color_palette_blue_2},'text' : df_2_ay_NPL['NPL'],'textposition':'auto', 'textfont':{'color':'white'},'hoverinfo' : 'x+text'}
-                                            ],
-                                    'layout':   {
-                                                    'height' : 175,
-                                                    'width' : 800,
-                                                    'title': ' Aylık NPL Aktarımları (M TL)', 
-                                                    'plot_bgcolor': 'rgba(0,0,0,0)' ,
-                                                    'paper_bgcolor': 'rgba(0,0,0,0)',
-                                                    'font': {'color': '#5D6D7E'},
-                                                    'yaxis': {'showgrid' : False, 'showline' : False, 'showticklabels' : False, 'range' : 'auto'},
-                                                    'xaxis': {'showgrid' : False, 'showline' : False, 'showticklabels' : True, 'tickvals': df_2_ay_NPL['Ay'], 'ticktext': df_2_ay_NPL['Ay']},
-                                                    'margin' : {'l': 40,'r': 40,'t': 40,'b': 70},
-                                                    'legend' : {'x': 1,'y': 0.5,'orientation': 'v', 'itemclick': 'toggleothers'}
-                                                }
-                                }
-
-    )
+    html.H1('Deneme')
     
     ])
 
