b672dad6474f0311acb319862aa7a6698141249c
codetomath <69963212+codetomath@users.noreply.github.com>
2020-08-28 11:51:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,7 @@ coordinates.drop_duplicates(subset ="Şehir", keep = "first", inplace = True)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets,meta_tags=[
-        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
-    ])
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 
